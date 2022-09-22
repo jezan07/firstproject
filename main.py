06cf5721ed72a665e673ae01b49a350737a83b57
jezan07 <ta.je.1997@gmail.com>
2022-09-22 20:55:20 +0300

    last homework

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
 
-name = input("your name:")
-#age= input("your age:")
-#address = input("your address:")
-
-#if name.isdigit():
-    #print("Hello Mr/Ms", name)
-#else:
-    #print("error")
-
+first_nummber = input("Enter your first number")
